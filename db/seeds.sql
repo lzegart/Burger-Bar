df82e791e89d27dddcf8b690615e3319412ea8c1
LZ <laraezegart@gmail.com>
2021-01-18 16:29:21 -0800

    added db folder with schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('The Batman Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Hulk Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Cheese Monster');
