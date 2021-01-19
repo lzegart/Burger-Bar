75c54db857da22af059b3a75e9dc9c6d8bd60de9
LZ <laraezegart@gmail.com>
2021-01-19 15:29:49 -0800

    added buttons and event listeners for devoured in burgers.js

---
+++ assets/js/burgers.js
@@ -0,0 +1,67 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+document.addEventListener('DOMContentLoaded', (event) => {
+    if (event) {
+        console.info('DOM loaded');
+    }
+
+    const devourBtn = document.querySelectorAll('.devour');
+
+    if (devourBtn) {
+        devourBtn.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                const id = e.target.getAttribute('data-id');
+                const devoured = e.target.getAttribute('data-devoured');
+
+                const burgerState = {
+                    devoured: true,
+                };
+
+                fetch(`/api/burgers/${id}`, {
+                    method: 'PUT',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+
+                    body: JSON.stringify(burgerState),
+                }).then((response) => {
+                    // Reload the page so the user can see the changes
+                    if (response.ok) {
+                        location.reload('/');
+                    } else {
+                        alert('Something went wrong...');
+                    }
+                });
+            });
+        });
+    }
+
+    const createBurgerBtn = document.getElementById('create-burger');
+
+    if (createBurgerBtn) {
+        createBurgerBtn.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const newBurger = {
+                burger_name: document.getElementById('burger-input').value.trim(),
+                devoured: document.getElementById('false').checked,
+            };
+
+            fetch('/api/burgers', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+
+                body: JSON.stringify(newBurger),
+            }).then(() => {
+                document.getElementById('burger-input').value = '';
+
+                // Reload the page so the user can see the new quote
+                console.log('Created a new burger!');
+                location.reload();
+            });
+        });
+    };
+});    
